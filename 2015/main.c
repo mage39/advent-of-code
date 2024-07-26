8c4f28f0e0679d779b1cf2da48fa36332fa22266
mage39 <106197576+mage39@users.noreply.github.com>
2024-07-25 21:05:16 -0500

    i would like to know why that guard clause fixed it, but day 2 part 1 complete

--- 2015/main.c
+++ 2015/main.c
@@ -9,8 +9,34 @@ int main () {
 	}
 
 	char line[10] = {0};
+	int total = 0, lc = 0;
 	
-	while (fgets()
+	while (fgets(line, 10, input)) {
+		if (!line[1]) break;
+		int length = 0, width = 0, height = 0, i = 0, slack = 0;
+		length = atoi(line);
+		for (; line[i] != 'x'; i++);
+		width = atoi(line + ++i);
+		for (; line[i] != 'x'; i++);
+		height = atoi(line + ++i);
+
+		if (length > width) {
+			if (length > height) {
+				slack = width * height;
+			} else {
+				slack = width * length;
+			}
+		} else {
+			if (width > height) {
+				slack = length * height;
+			} else {
+				slack = length * width;
+			}
+		}
+		total += 2 * length * width + 2 * width * height + 2 * height * length + slack;
+		lc++;
+	}
+	printf("%d, lc = %d\n", total, lc);
 
 	return 0;
 }
