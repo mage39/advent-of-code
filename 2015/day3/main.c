7fba18aba30d53f1cac01e7b7da6b4d08abcc629
mage39 <106197576+mage39@users.noreply.github.com>
2024-07-27 01:32:56 -0500

    added some error checking, moved a lot of logic into a function (the function name is basically a comment)

--- 2015/day3/main.c
+++ 2015/day3/main.c
@@ -17,18 +17,23 @@ uint64_t hashf (house data) {
 	return hash;
 }
 
-void rehashExpand (house* addrs, int* arrLen) { // aborts. i now suspect the infinite loop i undoubtedly created with my fancy gotos
+void rehashExpand (house* addrs, int* arrLen) { // aborts. no idea why
 	size_t size = sizeof(house) * *arrLen;
 	house* temp = malloc(size);
 	memcpy(temp, addrs, size);
 	*arrLen *= 2;
 	size *= 2;
-	addrs = realloc(addrs, size);
+	printf("%d\n", size);
+	house* err = realloc(addrs, size);
+	if (!err) {
+		printf("WTF\n");
+		exit(1);
+	} else addrs = err;
 	memset(addrs, 0, size);
 	for (int i = 0; i < *arrLen; i++) {
 		if (!temp[i].count) continue;
 		int j = hashf(temp[i]) % *arrLen;
-		for (int k = 0; k < *arrLen / 2; k++) {
+		for (int k = 0; k < *arrLen / 4; k++) {
 			int offset = j + k;
 			if (offset >= *arrLen) offset -= *arrLen;
 			if (!addrs[offset].count) {
@@ -38,6 +43,26 @@ void rehashExpand (house* addrs, int* arrLen) { // aborts. i now suspect the inf
 		}
 	}
 	free(temp);
+}
+
+int placeHash (house* addrs, house currentPos, int arrLen) {
+	int i = hashf(currentPos) % arrLen;
+	for (int j = 0; j < arrLen / 4; j++) {
+		int offset = i + j;
+		if (offset >= arrLen) offset -= arrLen;
+		if (addrs[offset].count) {
+			if (addrs[offset].addr[0] == currentPos.addr[0] && addrs[offset].addr[1] == currentPos.addr[1]) {
+				addrs[offset].count++;
+				return 0;
+			}
+		} else {
+			memcpy(addrs + offset, &currentPos, sizeof(house));
+			addrs[offset].count = 1;
+			currentPos.count++;
+			return 0;
+		}
+	}
+	return 1;
 }
 
 int main () {
@@ -50,6 +75,10 @@ int main () {
 	int arrLen = 32;
 	int letter = getc(input);
 	house* addrs = malloc(sizeof(house) * arrLen);
+	if (!addrs) {
+		printf("WTF\n");
+		exit(1);
+	}
 	memset(addrs, 0, sizeof(house) * arrLen);
 	house currentPos = {.count = 1};
 	while (letter != EOF) {
@@ -57,27 +86,8 @@ int main () {
 		if (letter == '>') currentPos.addr[1]++;
 		if (letter == 'v') currentPos.addr[0]--;
 		if (letter == '<') currentPos.addr[1]--;
-		failure:
-		int i = hashf(currentPos) % arrLen;
-		for (int j = 0; j < arrLen / 4; j++) {
-			int offset = i + j;
-			if (offset >= arrLen) offset -= arrLen;
-			if (addrs[offset].count) {
-				if (addrs[offset].addr[0] == currentPos.addr[0] && addrs[offset].addr[1] == currentPos.addr[1]) {
-					addrs[offset].count++;
-					goto success;
-				}
-			} else {
-				memcpy(addrs + offset, &currentPos, sizeof(house));
-				addrs[offset].count = 1;
-				currentPos.count++;
-				goto success;
-			}
-		}
-		// im just trying to get python's for..else here, ISTG im not doing gratuitous gotos
-		rehashExpand(addrs, &arrLen);
-		goto failure;
-		success:
+		int err = placeHash(addrs, currentPos, arrLen);
+		if (err) rehashExpand(addrs, &arrLen);
 		letter = getc(input);
 	}
 
